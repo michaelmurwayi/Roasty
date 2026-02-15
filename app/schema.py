4c05fcb9d648754568506cf715d593507b537e3d
MichaelMurwayix <michael.murwayix@gmail.com>
2026-02-15 09:17:13 +0300

    ft-extractor: Updating schema

--- app/schema.py
+++ app/schema.py
@@ -9,6 +9,15 @@ class CoffeeLot(BaseModel):
     pockets: int = 0
     weight_kg: float  # changed to match your JSON
 
+class FarmerDetails(BaseModel):
+    name: str
+    grower_code: str
+    account_number: str
+    bank: str
+    branch: str
+    swift_code: str = ""
+
 class ExtractedSaleData(BaseModel):
     buyer: str
     coffee_details: List[CoffeeLot]  # changed to match your JSON
+    # farmer_details: List[FarmerDetails]
